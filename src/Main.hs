577cf58054fe94ee11b51f77068ccd5fda605197
MarkArts <markarts@hoppinger.com>
2015-03-27 23:32:28 +0100

    Added functions to parse a folder to a certain depth

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Directory
 import Data.List
+import Data.Maybe
 
 testPath :: String
 testPath = "/home/mark/projects"
@@ -34,3 +35,26 @@ matchStringAgaints strings = or . map (flip isInfixOf strings )
 
 getWebsiteType :: WebsitePath -> IO WebsiteType
 getWebsiteType _ = return Wordpress
+
+type Name = String
+data PathTree = Folder Name [PathTree] | File String
+     deriving (Show)
+
+getContentsTill :: FilePath -> Int -> Int -> IO PathTree
+getContentsTill path depth maxDepth 
+    | depth >= maxDepth = return (Folder path [] )
+    | otherwise = do
+        paths <- getFilteredDirectoryContents path
+        subFolders <- mapM f ( map ((++) $ path ++ "/" ) paths )
+        return $ Folder path subFolders
+            where f p = do
+                        isFolder <- doesDirectoryExist $ p
+                        if isFolder then do
+                            subFolders <- getContentsTill p (depth+1) maxDepth
+                            return $ Folder p $ fromMaybe [] $ getContentFrom subFolders 
+                        else
+                            return $ File p
+
+getContentFrom :: PathTree -> Maybe [PathTree]
+getContentFrom (Folder _ ps)  = Just ps
+getContentFrom  _ = Nothing
