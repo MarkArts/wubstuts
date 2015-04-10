bdf7e17f28df093fcefea46fc306861ab679ac7b
Mats Rietdijk <matsrietdijk@gmail.com>
2015-04-11 00:47:39 +0200

    Clean up Settings with stylish-haskell

--- src/Settings.hs
+++ src/Settings.hs
@@ -2,11 +2,11 @@
 
 module Settings where
 
-import Data.Aeson
-import Control.Applicative
-import Control.Monad.State
+import           Control.Applicative
+import           Control.Monad.State
+import           Data.Aeson
 import qualified Data.ByteString.Lazy as B
-import Types
+import           Types
 
 settingsFile :: FilePath
 settingsFile = "config/settings.json"
@@ -15,47 +15,44 @@ settingsFile = "config/settings.json"
 data WebsiteFilter = WebsiteFilter {
     getFilterType :: WebsiteType,
     getConditions :: Conditions
-} deriving (Show)
+    } deriving (Show)
 
 data Settings = Settings {
-        getWebsitePath :: FilePath,
-        getSearchDepth :: Int,
-        getIgnoreFolders :: [FilePath],
-        getFilters :: [WebsiteFilter]
-} deriving (Show)
+    getWebsitePath   :: FilePath,
+    getSearchDepth   :: Int,
+    getIgnoreFolders :: [FilePath],
+    getFilters       :: [WebsiteFilter]
+    } deriving (Show)
 
 -- A stateful Settings monad to avoid parsing the
 -- entire file on each getSetting call
 type SettingsT = StateT (Maybe Settings) IO
 
 instance FromJSON Settings where
-  parseJSON (Object v) =
-    Settings <$>
-    (v .: "website_path")       <*>
-    (v .: "search_depth")       <*>
-    (v .: "ignore_folders")     <*>
-    (v .: "filters")
+    parseJSON (Object v) = do
+        Settings                    <$>
+            (v .: "website_path")   <*>
+            (v .: "search_depth")   <*>
+            (v .: "ignore_folders") <*>
+            (v .: "filters")
 
 instance FromJSON WebsiteFilter where
-  parseJSON (Object v) =
-    WebsiteFilter <$>
-    (v .: "type")             <*>
-    (v .: "conditions")
+    parseJSON (Object v) = do
+        WebsiteFilter           <$>
+            (v .: "type")       <*>
+            (v .: "conditions")
 
 -- Parse the Settings file
 parseSettings :: IO Settings
 parseSettings = do
     settingsContent <- B.readFile settingsFile
-    let settings = eitherDecode settingsContent :: Either String Settings
-    case settings of
+    case eitherDecode settingsContent of
         Left x -> error $ show x
         Right s -> return s
 
 -- Put the Settings into the SettingsT state monad
 getSettings :: SettingsT ()
-getSettings = do
-    settings <- liftIO parseSettings
-    put $ Just settings
+getSettings = liftIO parseSettings >>= put . Just
 
 -- Get Settings from the SettingsT
 -- Calling getSetting if the Settings are missing in SettingsT
