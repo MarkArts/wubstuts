33aca90688f47c931b8334ce344408ffab99f0bf
MarkArts <markarts@hoppinger.com>
2015-04-13 12:54:19 +0200

    adds wp plugin finding and parsing

--- src/Types/Wordpress.hs
+++ src/Types/Wordpress.hs
@@ -3,6 +3,9 @@ module Types.Wordpress where
 import Text.Parsec
 import Text.Parsec.ByteString (parseFromFile)
 import Data.Tree
+import Data.Maybe
+import System.FilePath.Posix
+import System.Directory
 import Data.ByteString (ByteString)
 import DirTree
 import Types
@@ -13,11 +16,10 @@ pluginsFolder = ["wp-content", "plugins"]
 versionFileLocation :: [FilePath]
 versionFileLocation = ["wp-includes", "version.php"]
 
--- todo: add error reporting
 wpVersion :: Website -> IO Version
 wpVersion (Website Wordpress _ _ t) = do
     case traverse t versionFileLocation of
-        Nothing -> return UnknownVersion
+        Nothing -> error "Couldn't find the versions file for"
         Just f -> do
             result <- parseFromFile wpParseVersionFile (rootLabel f)
             case result of
@@ -35,3 +37,57 @@ wpParseVersionFile = do
     versionVar = endOfLine >> spaces >> string "$wp_version"
     anyQuote = char '\'' <|> char '"'
     manyTillAnyQuote = manyTill anyChar anyQuote
+
+wpPlugins :: Website -> IO [Plugin]
+wpPlugins w@(Website Wordpress _ _ t) =
+    case traverse t pluginsFolder of
+        Nothing -> error $ "Couldn't find the modules folder for: " ++ (show w)
+        Just d -> do
+            let folders = [p | p@(Node _ (_:_)) <- subForest d]
+            mapM wpParsePluginFolder folders
+wpPlugins _ = error "Can't lookup Wordpress plugins for a non Wordpress website"
+
+-- we have to check all .php files because the plugin name can differ from the folder
+wpParsePluginFolder :: Tree FilePath -> IO Plugin
+wpParsePluginFolder t = do
+    possibles <- mapM wpFindPluginInfo (phpFiles t)
+    case catMaybes possibles of
+        [] -> error $ "couldn't find a wordpress plugin file in: " ++ (rootLabel t)
+        (x:_) -> return x
+    where phpFiles d = [ rootLabel p | p <- getChilds d ((==) ".php" . takeExtension . rootLabel) ]
+
+wpFindPluginInfo :: FilePath -> IO (Maybe Plugin)
+wpFindPluginInfo p = do
+                        fileExist <- doesFileExist p
+                        case fileExist of
+                            False -> error $ "Trying to parse non-existent file" ++ p
+                            True -> do
+                                plugin <- parseFromFile wpParsePluginInfo p
+                                case plugin of
+                                    Left e -> error $ show e
+                                    Right x -> return x
+
+-- we use lookAhead because the version and name may not be in that order
+wpParsePluginInfo :: Parsec ByteString () (Maybe Plugin)
+wpParsePluginInfo = do
+    name <- optionMaybe $ try $ lookAhead wpParsePluginName
+    case name of
+        Nothing -> return Nothing
+        Just n -> do
+            version <- optionMaybe $ try $ lookAhead wpParsePluginVersion
+            case version of
+                Just v -> return $ Just $ Plugin n (Version v)
+                Nothing -> return $ Just $ Plugin n UnknownVersion
+
+wpParsePluginName :: Parsec ByteString () String
+wpParsePluginName = wpInfoVariable "Plugin Name"
+
+wpParsePluginVersion :: Parsec ByteString () String
+wpParsePluginVersion = wpInfoVariable "Version"
+
+wpInfoVariable :: String -> Parsec ByteString () String
+wpInfoVariable v = do
+    manyTill anyChar (try $ string (v ++ ":"))
+    spaces
+    var <- manyTill anyChar endOfLine
+    return var
